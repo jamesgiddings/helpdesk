20960dd12db996c3d35a138918c48a0cf6209763
James Giddings <james.p.giddings@gmail.com>
2022-11-10 11:36:56 +0000

    inheritence issue with lombok fixed

--- help-desk-api/src/main/java/com/fdmgroup/helpdeskapi/model/request/AdminRequest.java
+++ help-desk-api/src/main/java/com/fdmgroup/helpdeskapi/model/request/AdminRequest.java
@@ -2,8 +2,10 @@ package com.fdmgroup.helpdeskapi.model.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 public class AdminRequest extends UserRequest {
 
